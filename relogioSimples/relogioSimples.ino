91679b27bec114907fbb10b3f594e87cf7c31117
HermannHernani <hermannhernani@outlook.com>
2020-02-17 12:03:28 -0400

    adicionando modulo de medição de bateria - modularizado

--- relogioSimples/relogioSimples.ino
+++ relogioSimples/relogioSimples.ino
@@ -21,6 +21,9 @@ bool LCD = true;
 RTC_TimeTypeDef RTC_TimeStruct;
 RTC_DateTypeDef RTC_DateStruct;
 
+float b, c = 0;
+int battery = 0;
+
 
 void buttons_code() {
   // Button A control the LCD (ON/OFF)
@@ -31,7 +34,7 @@ void buttons_code() {
       LCD = !LCD;
     } else {
       M5.Lcd.writecommand(ST7735_DISPON);
-      M5.Axp.ScreenBreath(255);
+      M5.Axp.ScreenBreath(10);
       LCD = !LCD;
     }
   }
@@ -47,11 +50,12 @@ void doTime() {
     M5.Lcd.setTextSize(2);
     M5.Rtc.GetTime(&RTC_TimeStruct);
     M5.Rtc.GetData(&RTC_DateStruct);
-    M5.Lcd.setCursor(27, 15);
+    M5.Lcd.setCursor(27, 25);
     M5.Lcd.printf("%02d:%02d:%02d\n", RTC_TimeStruct.Hours, RTC_TimeStruct.Minutes, RTC_TimeStruct.Seconds);
     M5.Lcd.setCursor(27, 50);
     M5.Lcd.setTextSize(1);
-    M5.Lcd.printf("Date: %04d-%02d-%02d\n", RTC_DateStruct.Year, RTC_DateStruct.Month, RTC_DateStruct.Date);
+    M5.Lcd.printf("Date: %02d-%02d-%04d\n", RTC_DateStruct.Date, RTC_DateStruct.Month, RTC_DateStruct.Year);
+    
   }
 }
 
@@ -108,21 +112,45 @@ void timeSync() {
       M5.Lcd.println("S Y N C");
       delay(500);
       M5.Lcd.fillScreen(BLACK);
-      M5.Lcd.setCursor(40, 0, 2);
-      M5.Lcd.println("Hermann");
-    }
+  }
+}
+
+void batteryLevel() {
+  M5.Lcd.setCursor(110, 3, 1);
+  c = M5.Axp.GetVapsData() * 1.4 / 1000;
+  b = M5.Axp.GetVbatData() * 1.1 / 1000;
+  //  M5.Lcd.print(b);
+  battery = ((b - 3.0) / 1.2) * 100;
+
+  if (c >= 4.5) {
+    M5.Lcd.print("CHG:");
+  }
+  else {
+    M5.Lcd.print("BAT:");
+  }
+
+  if (battery > 100)
+    battery = 100;
+  else if (battery < 100 && battery > 9)
+    M5.Lcd.print(" ");
+  else if (battery < 9)
+    M5.Lcd.print("  ");
+  if (battery < 10)
+    M5.Lcd.print("  ");
+
+  M5.Lcd.print(battery);
+  M5.Lcd.print("%");
 }
 
 void setup() {
   M5.begin();
 
-  M5.Lcd.setRotation(1);
+  M5.Lcd.setRotation(3);
   M5.Lcd.fillScreen(BLACK);
 
   M5.Lcd.setTextSize(1);
   M5.Lcd.setTextColor(WHITE,BLACK);
-  M5.Lcd.setCursor(40, 0, 2);
-  M5.Lcd.println("Hermann");
+
   //timeSync(); //uncomment if you want to have a timesync everytime you turn device on (if no WIFI is avail mostly bad)
 }
 
@@ -130,4 +158,5 @@ void loop() {
   M5.update();
   buttons_code();
   doTime();
+  batteryLevel();
 }
