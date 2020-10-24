5dec4c06ffe10e6c2286463c07c6a5bf6a0f0ca5
HermannHernani <hermannhernani@outlook.com>
2020-10-24 19:47:45 -0400

    Versão 1.0 - Monitor de Alergia

--- tracker/tracker.ino
+++ tracker/tracker.ino
@@ -8,6 +8,24 @@ int battery = 0;
 float b = 0;
 int state = 1;
 bool LCD = true;
+bool alertM = true;
+const int ledPin = 26;
+int randNumber;
+
+//WiFi credentials
+char* ssid       = "JACQUES HERNANI_2G";
+char* password   = "Hera2908"; 
+
+// database connection
+
+void sendData(){
+  IPAddress ip(127,0,0,1);
+  int photocellPin = 0;  // Analog input pin on Arduino we connected the SIG pin from sensor
+  int photocellReading;  // Here we will place our reading
+  char server[] = "127.0.0.1:3306"; // IMPORTANT: If you are using XAMPP you will have to find out the IP address of your computer and put it here (it is explained in previous article). If you have a web page, enter its address (ie. "www.yourwebpage.com")
+  char database[] = "person_data";
+  send = (hum;temp;1);
+}
 
 void batteryPercent() {
   M5.Lcd.setCursor(115, 2);
@@ -28,8 +46,8 @@ void batteryPercent() {
 }
 
 void buttons_code() {
-  // Button A control the LCD (ON/OFF)
-  if (M5.BtnA.wasPressed()) {
+  // Button B control the LCD (ON/OFF)
+  if (M5.BtnB.wasPressed()) {
     if (LCD) {
       M5.Lcd.writecommand(ST7735_DISPOFF);
       M5.Axp.ScreenBreath(0);
@@ -40,6 +58,102 @@ void buttons_code() {
       LCD = !LCD;
     }
   }
+  if (M5.BtnA.wasPressed()) {
+    M5.Lcd.writecommand(ST7735_DISPON);
+    M5.Axp.ScreenBreath(10);
+    M5.Lcd.setTextSize(1);
+    M5.Lcd.fillScreen(BLACK);
+    M5.Lcd.setCursor(20, 15);
+    M5.Lcd.println("Enviando/Conectando");
+    delay(1000);
+    M5.Lcd.fillScreen(BLACK);
+    M5.Lcd.setCursor(20, 15);
+    M5.Lcd.println("Aguarde");
+    delay(1000);
+    M5.Lcd.fillScreen(BLACK);
+    M5.Lcd.setCursor(20, 15);
+    M5.Lcd.println("Aguarde .");
+    delay(1000);
+    M5.Lcd.fillScreen(BLACK);
+    M5.Lcd.setCursor(20, 15);
+    M5.Lcd.println("Aguarde ..");
+    delay(1000);
+    M5.Lcd.fillScreen(BLACK);
+    M5.Lcd.setCursor(20, 15);
+    M5.Lcd.println("Aguarde ...");
+    delay(1000);
+    M5.Lcd.fillScreen(BLACK);
+    M5.Lcd.setCursor(20, 15);
+    M5.Lcd.println("Aguarde ....");
+    delay(2000);
+    M5.Lcd.fillScreen(BLACK);
+    M5.Lcd.setCursor(20, 15);
+    M5.Lcd.println("ENVIADO!");
+    delay(2000);
+    M5.Lcd.fillScreen(BLACK);
+    M5.Lcd.setCursor(5, 5, 2);
+    M5.Lcd.println("V1.0");
+    m5.update();
+    checkTemp();
+    //delay(60000);
+    delay(6000);
+    alert();
+    checkTemp();
+    M5.Lcd.setCursor(5, 5, 2);
+    M5.Lcd.println("V1.0");
+    batteryPercent();
+    sendData(hum, temp);
+  }
+}
+
+
+void alert(){
+  delay(1000);
+  M5.Lcd.fillScreen(RED);
+  M5.Lcd.setCursor(20, 15);
+  M5.Lcd.println("RISCO DE ALERGIA");
+  delay(1000);
+  M5.Lcd.fillScreen(BLUE);
+  M5.Lcd.setCursor(20, 15);
+  M5.Lcd.println("RISCO DE ALERGIA");
+  delay(1000);
+  M5.Lcd.fillScreen(RED);
+  M5.Lcd.setCursor(20, 15);
+  M5.Lcd.println("RISCO DE ALERGIA");
+  delay(1000);
+  M5.Lcd.fillScreen(BLUE);
+  M5.Lcd.setCursor(20, 15);
+  M5.Lcd.println("RISCO DE ALERGIA");
+  delay(1000);
+  M5.Lcd.fillScreen(RED);
+  M5.Lcd.setCursor(20, 15);
+  M5.Lcd.println("RISCO DE ALERGIA");
+  delay(1000);
+  M5.Lcd.fillScreen(BLUE);
+  M5.Lcd.setCursor(20, 15);
+  M5.Lcd.println("RISCO DE ALERGIA");
+  delay(1000);
+  M5.Lcd.fillScreen(RED);
+  M5.Lcd.setCursor(20, 15);
+  M5.Lcd.println("RISCO DE ALERGIA");
+  delay(1000);
+  M5.Lcd.fillScreen(BLUE);
+  M5.Lcd.setCursor(20, 15);
+  M5.Lcd.println("RISCO DE ALERGIA");
+  M5.Lcd.fillScreen(BLACK);
+  M5.Lcd.setCursor(5, 5, 2);
+  M5.Lcd.println("V1.0");
+}
+
+void checkTemp(){
+  // put your main code here, to run repeatedly:
+  float tmp = dht12.readTemperature();
+  float hum = dht12.readHumidity();
+  M5.Lcd.setCursor(5, 20, 2);
+  M5.Lcd.printf("Temp: %2.0f", tmp);
+  M5.Lcd.setCursor(5, 40, 2);
+  M5.Lcd.printf("Humi: %2.0f%%", hum);
+  batteryPercent();
 }
 
 void setup() {
@@ -48,19 +162,14 @@ void setup() {
   Wire.begin(0,26);
   M5.Lcd.setRotation(1);
   M5.Lcd.fillScreen(BLACK);
-  M5.Lcd.setCursor(0, 0, 2);
-  M5.Lcd.println("V0.3");
+  M5.Lcd.setCursor(5, 5, 2);
+  M5.Lcd.println("V1.0");
 }
 
 void loop() {
   M5.update();
+  pinMode (ledPin, OUTPUT);
   buttons_code();
-  // put your main code here, to run repeatedly:
-  float tmp = dht12.readTemperature();
-  float hum = dht12.readHumidity();
-  M5.Lcd.setCursor(0, 20, 2);
-  M5.Lcd.printf("Temp: %2.1f", tmp);
-  M5.Lcd.setCursor(0, 40, 2);
-  M5.Lcd.printf("Humi: %2.0f%%", hum);
+  checkTemp();
   batteryPercent();
 }
